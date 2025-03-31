9a38ec06be50a812604a17fd0b811c8e40af3a7c
Tanzeel-Azeem <azeemtanzeel39@gmail.com>
2025-03-31 20:54:25 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import Image from "next/image";
-// import Comment from "./photos";
 
-interface Idata {
-  id : number
-  message : string,
-  url : string
-}
 
 export default async function Home() {
 
-
-  
   return (
    <div>
     <h1>hi</h1>
